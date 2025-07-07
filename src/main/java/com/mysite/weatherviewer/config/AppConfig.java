e534fc0da5338dc23d706f6971875b1d70da1683
Danila Grishin <spacecowboy790@gmail.com>
2025-07-07 23:17:58 +0700

    Add Liquibase

--- src/main/java/com/mysite/weatherviewer/config/AppConfig.java
+++ src/main/java/com/mysite/weatherviewer/config/AppConfig.java
@@ -1,18 +1,19 @@
 package com.mysite.weatherviewer.config;
 
+import java.util.Properties;
+import javax.sql.DataSource;
+import liquibase.integration.spring.SpringLiquibase;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -49,6 +50,9 @@ public class AppConfig {
     @Value("${hibernate.temp.use-jdbc-metadata-defaults}")
     private String hibernateTempUseJdbcMetadataDefaults;
 
+    @Value("${liquibase.change-log}")
+    private String liquibaseChangeLog;
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
@@ -61,22 +65,36 @@ public class AppConfig {
         dataSource.setUsername(databaseUsername);
         dataSource.setPassword(databasePassword);
         dataSource.setDriverClassName(databaseDriverClassName);
+
         return dataSource;
     }
 
+    @Bean("liquibase")
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource());
+        liquibase.setChangeLog(liquibaseChangeLog);
+
+        return liquibase;
+    }
+
     @Bean
+    @DependsOn("liquibase")  // Указываем зависимость
     public LocalSessionFactoryBean sessionFactory() {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(dataSource());
         sessionFactory.setPackagesToScan("com.mysite.weatherviewer.entity");
         sessionFactory.setHibernateProperties(hibernateProperties());
+
         return sessionFactory;
     }
+
 
     @Bean
     public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
         HibernateTransactionManager transactionManager = new HibernateTransactionManager();
         transactionManager.setSessionFactory(sessionFactory);
+
         return transactionManager;
     }
 
@@ -88,6 +106,7 @@ public class AppConfig {
         properties.put("hibernate.hbm2ddl.auto", hibernateHbm2ddlAuto);
         properties.put("hibernate.jdbc.lob.non_contextual_creation", hibernateLobNonContextualCreation);
         properties.put("hibernate.temp.use_jdbc_metadata_defaults", hibernateTempUseJdbcMetadataDefaults);
+
         return properties;
     }
 }
